c6b03e9c4e3b58fa9194a705219440c19c96fb76
LoopFreedom <yangchengjiey@gmail.com>
2015-03-01 17:15:35 -0800

    Updated Admin Info

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
@@ -5,7 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
+//upated by looperXD
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
 @CommandParameters(
         description = "See how to become admin.",
@@ -17,11 +17,12 @@ public class Command_admininfo extends TFM_Command
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
         TFM_Util.playerMsg(sender_p, "So you wish to apply for admin do you?", ChatColor.BLUE);
-        TFM_Util.playerMsg(sender_p, "Go to our forums at http://freedomop.boards.net", ChatColor.GREEN);
-        TFM_Util.playerMsg(sender_p, "Create a thread in the Super Admin applications forum following the template:", ChatColor.GREEN);
-        TFM_Util.playerMsg(sender_p, "http://freedomop.boards.net/thread/249/admin-application-template-revised-july", ChatColor.GREEN);
-        TFM_Util.playerMsg(sender_p, "wait for admin responses on your application and an eventual verfdict.", ChatColor.GREEN);
-        TFM_Util.playerMsg(sender_p, "Don't ask for admins to look at the application, it will lower your chances of being accepted.", ChatColor.RED);
+        TFM_Util.playerMsg(sender_p, "All Apps are on Hold, We are having a Admin Freeze", ChatColor.BLUE);
+        //TFM_Util.playerMsg(sender_p, "Go to our forums at http://freedomop.boards.net", ChatColor.GREEN);
+        //TFM_Util.playerMsg(sender_p, "Create a thread in the Super Admin applications forum following the template:", ChatColor.GREEN);
+        //TFM_Util.playerMsg(sender_p, "http://freedomop.boards.net/thread/249/admin-application-template-revised-july", ChatColor.GREEN);
+        //TFM_Util.playerMsg(sender_p, "wait for admin responses on your application and an eventual verfdict.", ChatColor.GREEN);
+       // TFM_Util.playerMsg(sender_p, "Don't ask for admins to look at the application, it will lower your chances of being accepted.", ChatColor.RED);
         return true;
     }
 }
